472654ce96d81bdf2d315d19621ed47720ed1964
Christien Kelly <christien@ridewithshare.com>
2021-04-02 06:48:46 -0400

    created custom signin button.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 
@@ -43,7 +44,7 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         label="Email"
                         required />  
-                        
+
                     <FormInput 
                         name="password" 
                         type="password"
@@ -52,10 +53,7 @@ class SignIn extends React.Component {
                         label="Password"
                         required /> 
 
-                    <input 
-                        type="submit"
-                        value="Submit Form"
-                    />
+                    <CustomButton type="submit"> Sign In </CustomButton>
                 </form>
             
             
